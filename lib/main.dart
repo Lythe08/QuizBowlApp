f272c448c12f1ace9797638e75e7e842f2375aa9
AJ <73261576+SpectreOfDeath@users.noreply.github.com>
2024-09-19 10:03:28 -0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+//Written by Lysander Pineapple
+
 void main() {
   runApp(MyApp());
 }
