501301e1f8635475fe971adeb58df1631e8ab495
lysander <lysander.pineda@gmail.com>
2024-09-26 10:25:37 -0700

    namer app

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'Quiz Bowl App',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan.shade400),
